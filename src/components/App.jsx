3556a15b7dc778a75ef09bd17c734515bb1a4b11
Antony Cabeza <antonycabeza@gmail.com>
2021-08-19 22:37:38 -0400

    Modificando git hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import "../styles/components/App.scss";
+import '../styles/components/App.scss';
 
-const App = () => <h1>Hola mundo</h1>;
+const App = () => <h1>Epa mundo</h1>;
 
 export default App;
