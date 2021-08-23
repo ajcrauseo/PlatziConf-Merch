6b4f4f9af4099824dc42b2a38e125ace3f4c2423
Antony Cabeza <antonycabeza@gmail.com>
2021-08-23 11:00:07 -0400

    Conexion React Router. Iconos y estilos

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,20 @@
 /* eslint-disable arrow-body-style */
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 import '../styles/components/Header.scss';
 
 const Header = () => {
   return (
     <div className="Header">
-      <h1 className="Header-title">PlatziConf Merch</h1>
-      <div className="Header-checkout">Checkout</div>
+      <h1 className="Header-title">
+        <Link to="/">PlatziConf Merch</Link>
+      </h1>
+      <div className="Header-checkout">
+        <Link to="/checkout">
+          <i className="fas fa-shopping-cart" />
+        </Link>
+      </div>
     </div>
   );
 };
